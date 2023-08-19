31dbf186d332b468fb6cb16f09ae8cd1cc1697d3
metin <metinsucu42@hotmail.com>
2023-08-20 02:24:56 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+class SvgSpriteGenerator
+{
+    private $sourceDir;
+    private $outputFile;
+    private $excludeFiles;
+    private $includeFiles;
+
+    public function __construct($sourceDir, $outputFile, $excludeFiles = [], $includeFiles = [])
+    {
+        $this->sourceDir = $sourceDir;
+        $this->outputFile = $outputFile;
+        $this->excludeFiles = $excludeFiles;
+        $this->includeFiles = $includeFiles;
+    }
+
+    public function generateSprite()
+    {
+        $svgFiles = $this->scanDirectory($this->sourceDir);
+
+        if (empty($svgFiles)) {
+            echo "Klasörde SVG dosyası bulunamadı.";
+            exit;
+        }
+
+        $svgSprite = '<svg width="0" height="0" class="hidden iconset">'."\n";
+        foreach ($svgFiles as $svgFile) {
+            $svgName = pathinfo($svgFile, PATHINFO_FILENAME);
+            $svgContent = file_get_contents($svgFile);
+
+            preg_match('/viewBox="([^"]+)"/', $svgContent, $viewBoxMatches);
+            $viewBox = $viewBoxMatches[1] ?  $viewBoxMatches[1]: '0 0 100 100';
+
+            $svgContent = preg_replace('/\s(data-name|fill|stroke)="[^"]+"/', '', $svgContent);
+            $svgContent = preg_replace('/<svg[^>]+>/', '', $svgContent);
+            $svgContent = preg_replace('/<\/svg>/', '', $svgContent);
+
+            if (!preg_match('/viewBox="/i', $svgContent)) {
+                $svgContent = preg_replace('/<svg/i', '<svg viewBox="0 0 100 100"', $svgContent, 1);
+            }
+            $svgContent=trim($svgContent);
+
+            $svgSprite .= "<symbol id=\"$svgName\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"$viewBox\">\n$svgContent\n</symbol>\n";
+        }
+        $svgSprite .= '</svg>';
+
+        file_put_contents($this->outputFile, $svgSprite);
+
+        echo "SVG sprite dosyası oluşturuldu: {$this->outputFile}";
+    }
+
+    private function scanDirectory($dir)
+    {
+        $svgFiles = [];
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if ($file === '.' || $file === '..') {
+                continue;
+            }
+            $filePath = $dir . '/' . $file;
+            if (is_file($filePath) && pathinfo($file, PATHINFO_EXTENSION) === 'svg') {
+                $filename = pathinfo($file, PATHINFO_FILENAME);
+                if (
+                    (!empty($this->includeFiles) && in_array($filename, $this->includeFiles)) ||
+                    (empty($this->includeFiles) && !in_array($filename, $this->excludeFiles))
+                ) {
+                    $svgFiles[] = $filePath;
+                }
+            }
+        }
+        return $svgFiles;
+    }
+}
+
+// Kullanım örneği
+$sourceDir = __DIR__ . "/svg/";
+$outputFile = 'svgsprite.svg';
+$excludeFiles = ['excluded'];// Dizindeki hariç tutulacak dosyalar.
+$includeFiles = []; // Dizindeki eklenecek dosyalar ( boş bırakılırsa tüm dizini tarar )
+
+$spriteGenerator = new SvgSpriteGenerator($sourceDir, $outputFile, $excludeFiles, $includeFiles);
+$spriteGenerator->generateSprite();
